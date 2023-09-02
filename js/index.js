344619bace53480339988cae5470f988fca15cf2
iftyzubaer <iftyzubaer1304.com>
2023-09-02 12:57:08 +0600

    default view

--- js/index.js
+++ js/index.js
@@ -9,31 +9,24 @@ const loadBtn = async () => {
 
 const displayButtons = buttons => {
       const buttonContainer = document.getElementById('button-container');
-
-      for (const button of buttons) {
-            const buttonCard = document.createElement('button');
+      buttons.forEach(button => {
+            const buttonCard = document.createElement('div');
+            const buttonCardId = button.category_id;
             buttonCard.classList = `btn btn-error text-white bg-[#FF1F3D]`;
             buttonCard.innerHTML = `
-            ${button.category}
+            <button onclick="sortHandler('${buttonCardId}')">${button.category}</button>
             `;
             buttonContainer.appendChild(buttonCard);
-      }
-}
-
-const loadVideo = async () => {
-      const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000')
-      const data = await res.json();
-      const videos = data.data;
-      displayVideo(videos);
+      })
 }
 
 const displayVideo = videos => {
 
       const videoContainer = document.getElementById('video-container');
+      videoContainer.innerHTML = "";
 
       videos.forEach(video => {
             const videoCard = document.createElement('div');
-
             const seconds = video.others.posted_date;
             const hours = Math.floor(seconds / 3600);
             const remainingSeconds = seconds % 3600;
@@ -92,11 +85,13 @@ const displayVideo = videos => {
       })
 }
 
-const sortHandler = () => {
-      const sort = document.getElementById('buttonCardId')
-      console.log(sort);
+const sortHandler = async (buttonCardId) => {
+      const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${buttonCardId}`);
+      const data = await res.json();
+      const videos = data.data;
+      displayVideo(videos);
 }
 
-loadVideo();
+sortHandler("1000");
 
 loadBtn();
